adc0360ddb23919a050c0416c6068e798a53c610
Alan R <alanribeiroandrade@gmail.com>
2023-01-30 20:38:15 +0100

    HTML escaping

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-	return "<p>Hello world!</p>"
+@app.route("/<name>")
+def hello_world(name):
+	return f"<p>Hello {escape(name)}!</p>"
