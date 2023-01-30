d7ecc5b5884b00321dccb2ca7fbca9e9d35d3a1d
Alan R <alanribeiroandrade@gmail.com>
2023-01-30 20:53:34 +0100

    Variable rules

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -3,9 +3,17 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello_world(name):
-	return f"<p>Hello {escape(name)}!</p>"
+@app.route('/user/<username>')
+def show_user_profile(username):
+	return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	return f'Post {escape(post_id)}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+	return f'Subpath {escape(subpath)}'
 
 @app.route('/')
 def index():
